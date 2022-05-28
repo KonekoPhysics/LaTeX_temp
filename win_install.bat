24bc5af49294fb6fbd134575f28e680918caff92
KonekoPhysics <89322889+KonekoPhysics@users.noreply.github.com>
2022-05-28 22:09:59 +0900

    Initial commit

---
+++ win_install.bat
@@ -0,0 +1,6 @@
+copy preamble.sty C:\texlive\texmf-local\tex\platex\
+copy prechemistry.sty C:\texlive\texmf-local\tex\platex\
+copy prefonts.sty C:\texlive\texmf-local\tex\platex\
+copy preprog.sty C:\texlive\texmf-local\tex\platex\
+cd C:\texlive\texmf-local\tex\platex\
+mktexlsr
