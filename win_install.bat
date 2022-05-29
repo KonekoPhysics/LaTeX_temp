7931062e771fa6b850864a34a99721750bdf5d12
KonekoPhysics <89322889+KonekoPhysics@users.noreply.github.com>
2022-05-29 11:27:53 +0900

    version2.2

--- win_install.bat
+++ win_install.bat
@@ -1,4 +1,5 @@
 copy preamble.sty C:\texlive\texmf-local\tex\platex\
+copy jpreamble.sty C:\texlive\texmf-local\tex\platex\
 copy prechemistry.sty C:\texlive\texmf-local\tex\platex\
 copy prefonts.sty C:\texlive\texmf-local\tex\platex\
 copy preprog.sty C:\texlive\texmf-local\tex\platex\
