d4fcd40e2e6dd0ab8f513fcffc74c9f831a80308
KonekoPhysics <89322889+KonekoPhysics@users.noreply.github.com>
2022-05-30 19:06:06 +0900

    updatingwininstall

--- win_install.bat
+++ win_install.bat
@@ -1,3 +1,10 @@
+whoami /priv | find "SeDebugPrivilege" > nul
+if %errorlevel% neq 0 (
+　@powershell start-process ”%~0" -verb runas
+　echo You're not administrator
+　exit
+)
+echo Made you administrator
 copy preamble.sty C:\texlive\texmf-local\tex\platex\
 copy jpreamble.sty C:\texlive\texmf-local\tex\platex\
 copy prechemistry.sty C:\texlive\texmf-local\tex\platex\
@@ -5,3 +12,5 @@ copy prefonts.sty C:\texlive\texmf-local\tex\platex\
 copy preprog.sty C:\texlive\texmf-local\tex\platex\
 cd C:\texlive\texmf-local\tex\platex\
 mktexlsr
+pause
+exit
